46bf98cc75fac61e152c21e47e710862990d1107
Lucas <lcredie@vailsys.com>
2020-05-01 12:00:52 -0500

    implemented sms handler.

---
+++ internal/server/middleware/middleware.go
@@ -0,0 +1,15 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+const LoggerKey string = "logger"
+
+func SetupLogger(logger *zap.Logger) gin.HandlerFunc {
+	return func(c *gin.Context) {
+			requestLog := logger.With(zap.String("path", c.Request.URL.Path), zap.String("method", c.Request.Method))
+			c.Set(LoggerKey, requestLog)
+	}
+}
